79f8e23fe90c8ccaa6bfa15f389aff2f7c689ce3
Interfiber <webmaster@interfiber.dev>
2022-03-13 19:50:17 -0400

    fix color emojis

--- dmenu/config.h
+++ dmenu/config.h
@@ -4,14 +4,15 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"monospace:size=10",
+        "Noto Color Emoji:pixelsize=10:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#4c566a", "#0f111A" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeSel] = { "#4c566a", "#3b4252" },
+	[SchemeOut] = { "#4c566a", "#3b4252" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
