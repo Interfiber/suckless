b1eec905efe5b6ff66709d6296949eeb2b631dcc
Interfiber <webmaster@interfiber.dev>
2022-03-21 06:35:45 -0400

    update dmenu

--- dmenu/config.h
+++ dmenu/config.h
@@ -10,9 +10,9 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#4c566a", "#0f111A" },
-	[SchemeSel] = { "#4c566a", "#3b4252" },
-	[SchemeOut] = { "#4c566a", "#3b4252" },
+	[SchemeNorm] = { "#e5e9f0", "#00010a" },
+	[SchemeSel] = { "#e5e9f0", "#ff4151" },
+	[SchemeOut] = { "#e5e9f0", "#00010a" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
